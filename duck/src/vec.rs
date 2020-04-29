fedb63463d44bb492ec80d1b28973ba6fa572ebf
Martin Carltoft <martin@bitbleep.com>
2020-04-29 20:44:06 +0200

    Implement more functions from std vec

--- duck/src/vec.rs
+++ duck/src/vec.rs
@@ -11,6 +11,7 @@ impl<T> Vec<T>
 where
     T: Copy,
 {
+    #[inline(always)]
     pub fn new(data: &'static mut [T], lock_flag: &'static core::sync::atomic::AtomicBool) -> Self {
         Self {
             data,
@@ -19,20 +20,59 @@ where
         }
     }
 
-    pub fn len(&self) -> usize {
-        self.offset
-    }
-
+    #[inline(always)]
     pub fn capacity(&self) -> usize {
         self.data.len()
     }
 
-    pub fn clear(&mut self) {
-        self.offset = 0;
+    // pub fn truncate(&mut self, len: usize)
+
+    #[inline(always)]
+    pub fn as_slice(&self) -> &[T] {
+        &self.data[..self.offset]
+    }
+
+    #[inline(always)]
+    pub fn as_mut_slice(&mut self) -> &mut [T] {
+        &mut self.data[..self.offset]
+    }
+
+    #[inline(always)]
+    pub fn as_ptr(&self) -> *const T {
+        self.data.as_ptr()
+    }
+
+    #[inline(always)]
+    pub fn as_mut_ptr(&mut self) -> *mut T {
+        self.data.as_mut_ptr()
+    }
+
+    // pub fn swap_remove(&mut self, index: usize) -> T
+
+    pub fn insert(&mut self, index: usize, element: T) {
+        if self.len() == self.capacity() {
+            panic!("cannot grow");
+        }
+        if index >= self.capacity() {
+            panic!("out of bounds");
+        }
+        self.data.copy_within(index..self.offset, index + 1);
+        self.data[index] = element;
+        self.offset += 1;
+    }
+
+    pub fn remove(&mut self, index: usize) -> T {
+        if index >= self.len() {
+            panic!("out of bounds");
+        }
+        let element = self.data[index];
+        self.data.copy_within(index + 1..self.offset, index);
+        self.offset -= 1;
+        element
     }
 
     pub fn push(&mut self, value: T) {
-        if self.offset == self.capacity() {
+        if self.len() == self.capacity() {
             panic!("cannot grow");
         }
         self.data[self.offset] = value;
@@ -47,6 +87,30 @@ where
                 Some(self.data[self.offset])
             }
         }
+    }
+
+    pub fn append(&mut self, other: &mut Vec<T>) {
+        let space = self.capacity() - self.len();
+        if space < other.len() {
+            panic!("cannot grow");
+        }
+        self.data[self.offset..self.offset + other.len()].copy_from_slice(other.as_slice());
+        self.offset += other.len();
+    }
+
+    #[inline(always)]
+    pub fn clear(&mut self) {
+        self.offset = 0;
+    }
+
+    #[inline(always)]
+    pub fn len(&self) -> usize {
+        self.offset
+    }
+
+    #[inline(always)]
+    pub fn is_empty(&self) -> bool {
+        self.offset == 0
     }
 
     pub fn iter(&self) -> core::slice::Iter<T> {
